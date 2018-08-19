ab6a2cb13a87bd773f2f21963672e3da26e0756d
Kevin <kphyde@gmail.com>
2018-08-19 13:47:07 -0700

    get onkey event to work and write to console and wrongGuesses area in html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,19 +9,36 @@ let game = {
         "Doc Holliday"
     ],
 
-    "currentAnswer" : function() {
+    "setGame" : function() {
+        // randopmly select string from "answers" array;
         // convert string to array;
     },
 
-    "answerZone" : [
-        "_","_","t","_","K"
-    ],
+    "answerZone" : [],
 
-    "newGuess" : //onkey event
-    ,
+    "newGuess" : "hold", //onkey event
 
     "attempts" : 0,
 
     "wrongGuesses" : [],
+
+}
+
+let html_wrong_guesses = document.getElementById("wrong_guesses");
+
+
+alert("javascrip loaded");
+
+document.onkeyup = function(event) {
+    // setting userInput
+    let userInput = event.key;
+    game.newGuess = userInput
+    console.log("newGuess set to: " + game.newGuess);
+
+    // writing to answerZone   -  update to run function to check for correct, only push to array if incorrect
+    game.answerZone.push(userInput.toUpperCase());
+    console.log("answerZone state: " + game.answerZone)
+
+    html_wrong_guesses.textContent = game.answerZone; // change to format not as an array
 
 }
