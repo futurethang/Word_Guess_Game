88c08de29f323fb86e192871a2e841384890dfe7
Kevin <kphyde@gmail.com>
2018-08-19 12:34:20 -0700

    psuedo code in txt doc, begin guilding game object

---
+++ assets/javascript/game.js
@@ -0,0 +1,27 @@
+let game = {
+    "answers" : [
+        "Wyatt Earp",
+        "Billy the Kid",
+        "Buffalo Bill",
+        "Butch Cassidy",
+        "Bill Pickett",
+        "Dalton Gang",
+        "Doc Holliday"
+    ],
+
+    "currentAnswer" : function() {
+        // convert string to array;
+    },
+
+    "answerZone" : [
+        "_","_","t","_","K"
+    ],
+
+    "newGuess" : //onkey event
+    ,
+
+    "attempts" : 0,
+
+    "wrongGuesses" : [],
+
+}
